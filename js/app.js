0bf2cd3a9021fcd2a53e4ed5247fc93f2d5d31db
CelsoParedes_sng <celso.paredes@sngular.team>
2016-06-06 11:50:46 +0200

    Portfolio modal done and working, contact in to-do-list

--- js/app.js
+++ js/app.js
@@ -1,27 +1,45 @@
 (function(){
 
-	var app = angular.module('portfolio', []);
+	var app = angular.module('portfolio', ['ui.bootstrap']);
+
+
+	//Controlador para cargar los modales cuando se clicke en una obra.
+	app.controller('ModalInstanceCtrl', function($scope, $modalInstance, value) {
+	  $scope.data = {};
+	  $scope.data.value = value;
+
+	  $scope.ok = function () {
+	  	$modalInstance.close('this is result for close');
+	  };
+	  
+	});
+
+	app.controller('portfolioCtrl', function($scope, $modal){
+		this.portfolioData = portfolioImagesAndSubheaders;
+		$scope.open = function(_value) {
+		    var modalInstance = $modal.open({
+		      controller: "ModalInstanceCtrl",
+		      templateUrl: 'myModal',
+		      resolve: {
+		        value: function() {
+		          return _value;
+		        }
+		      }
+		    });
+		    modalInstance.result
+	  	}
+	});
 
 	app.directive('portfolioItem', function(){
 		return{
 			restrict: 'E',
 			templateUrl: 'portfolio-item.html',
-			controller: function(){
-				this.portfolioData = portfolioImagesAndSubheaders;
-				this.openModal = function(){
-					alert('Hola');
-				}
-			},
+			controller: 'portfolioCtrl',
 			controllerAs: 'port'
 		}
 	});
 
-	//Directiva para cargar los modales cuando se clicke en una obra.
-	app.controller('portfolioModal', function(){
-		this.openModal = function(){
-			alert('Hola');
-		}
-	});
+
 
 	app.controller('formController',function($http){
 		/*this.emai = {};
@@ -43,12 +61,27 @@
 	});
 
 	var portfolioImagesAndSubheaders = [
-	{i: 'css/img/portfolio/roundicons.png',h4: 'Round Icons', p: 'Graphic Design'},
-	{i: 'css/img/portfolio/startup-framework.png',h4: 'Startup Framework', p: 'Website Design'},
-	{i: 'css/img/portfolio/treehouse.png',h4: 'Treehouse', p: 'Website Design'},
-	{i: 'css/img/portfolio/golden.png',h4: 'Golden', p: 'Website Design'},
-	{i: 'css/img/portfolio/escape.png',h4: 'Escape', p: 'Website Design'},
-	{i: 'css/img/portfolio/dreams.png',h4: 'Dreams', p: 'Website Design'}
+	{i: 'css/img/portfolio/roundicons.png',h4: 'Round Icons', p: 'Graphic Design', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!'},
+
+
+
+	{i: 'css/img/portfolio/startup-framework.png',h4: 'Startup Framework', p: 'Website Design', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!'},
+
+
+
+	{i: 'css/img/portfolio/treehouse.png',h4: 'Treehouse', p: 'Website Design', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!'},
+
+
+
+	{i: 'css/img/portfolio/golden.png',h4: 'Golden', p: 'Website Design', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!'},
+
+
+
+	{i: 'css/img/portfolio/escape.png',h4: 'Escape', p: 'Website Design', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!'},
+
+
+
+	{i: 'css/img/portfolio/dreams.png',h4: 'Dreams', p: 'Website Design', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!'}
 	];
 
 	var emails = [
